9e577e248eb7a993b5d3ca266d0ae15f8bec89b5
victorkiprono <kipronovictormutai@gmail.com>
2021-10-24 14:35:41 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Cocktail Bar is a pro-African inspired bar and restaurant by the people 
+        of the people with well-known tales that revelers celebrate. 
+        It's beyond creative and the energy and vibe really matter to its city. 
+        The drinks are excellent to give one hell of a kicker night.
+      </p>
+    </section>
+  );
+}
